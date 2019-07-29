3e0b82d8da70fd046f4197d54cecab25dc4217a7
Ekaterina Ivanova <ivanova.ecaterina@yandex.ru>
2019-07-30 01:42:06 +0300

    created components Blog.js and Post.js

---
+++ src/app/components/WelcomeModal.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default class WelcomeModal extends React.Component {
+    render() {
+        return (
+            <div>Приветственное окно</div>
+        );
+    }
+}
